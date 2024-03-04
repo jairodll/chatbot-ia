40cc3279f05eab09f44b358c841380d26732a01a
jairo <jairo_deleon97@hotmail.com>
2024-03-03 20:59:13 -0600

    Se agrega funcionalidad inicial de ChatBot

---
+++ src/main/java/com/ia/chatbotia/controller/CustomBotController.java
@@ -0,0 +1,32 @@
+package com.ia.chatbotia.controller;
+
+import com.ia.chatbotia.dto.ChatGPTResponse;
+import com.ia.chatbotia.dto.ChatGPTRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/bot")
+public class CustomBotController {
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt){
+        ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+        ChatGPTResponse chatGptResponse = template.postForObject(apiURL, request, ChatGPTResponse.class);
+        return chatGptResponse.getChoices().get(0).getMessage().getContent();
+    }
+}
---
+++ src/main/java/com/ia/chatbotia/dto/ChatGPTResponse.java
@@ -0,0 +1,24 @@
+package com.ia.chatbotia.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatGPTResponse {
+
+    private List<Choice> choices;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+        private int index;
+        private Message message;
+    }
+}
