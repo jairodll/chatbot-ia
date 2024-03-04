0521d421b7c18d8b5d376ce708cb757923553760
jairo <jairo_deleon97@hotmail.com>
2024-03-03 20:20:25 -0600

    Initial commit

---
+++ src/main/java/com/ia/chatbotia/ChatbotIaApplication.java
@@ -0,0 +1,13 @@
+package com.ia.chatbotia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ia/chatbotia/ChatbotIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.chatbotia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
