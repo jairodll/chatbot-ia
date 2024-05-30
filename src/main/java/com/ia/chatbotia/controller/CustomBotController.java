1a9a19835acaf140e126e488f63574614233a49c
OscarAndresEO <81129116+OscarAndresEO@users.noreply.github.com>
2024-05-30 17:15:23 -0600

    modificacion en el servicio de ia
    se modifica a un metodo post el servicio que consume la IA

--- src/main/java/com/ia/chatbotia/controller/CustomBotController.java
+++ src/main/java/com/ia/chatbotia/controller/CustomBotController.java
@@ -2,9 +2,12 @@ package com.ia.chatbotia.controller;
 
 import com.ia.chatbotia.dto.ChatGPTResponse;
 import com.ia.chatbotia.dto.ChatGPTRequest;
+import com.ia.chatbotia.dto.mensajeDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +26,10 @@ public class CustomBotController {
     @Autowired
     private RestTemplate template;
 
-    @GetMapping("/chat")
-    public String chat(@RequestParam("prompt") String prompt){
-        ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+    @PostMapping("/chat")
+    public String chat(@RequestBody mensajeDto datos){
+        System.out.println("mensaje::  "+datos.getMensaje());
+        ChatGPTRequest request = new ChatGPTRequest(model, datos.getMensaje());
         ChatGPTResponse chatGptResponse = template.postForObject(apiURL, request, ChatGPTResponse.class);
         return chatGptResponse.getChoices().get(0).getMessage().getContent();
     }
---
+++ src/main/java/com/ia/chatbotia/dto/mensajeDto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ia.chatbotia.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author Oscar
+ */
+@Data
+@AllArgsConstructor
+@lombok.NoArgsConstructor
+public class mensajeDto {
+    String mensaje; 
+}
